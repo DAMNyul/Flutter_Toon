d630e9232f09ed229c31328b414b9dac8362e8f5
김담율 <s24001@gsm.hs.kr>
2024-08-28 22:13:22 +0900

    플러터 공부

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,3 +1,5 @@
+import "dart:convert";
+
 import "package:http/http.dart" as http;
 
 class ApiServices {
@@ -8,7 +10,7 @@ class ApiServices {
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
+      jsonDecode(response.body);
       return;
     }
     throw Error();
