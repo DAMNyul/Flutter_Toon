e803deb9aa5df844d9a69d3df0ec741affa2a4d2
김담율 <s24001@gsm.hs.kr>
2024-09-02 16:03:36 +0900

    flutter다시 감잡기

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -55,14 +55,36 @@ class HomeScreen extends StatelessWidget {
 
         return Column(
           children: [
-            Image.network(
-              webtoon.thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    offset: const Offset(0, 0),
+                    color: Colors.black.withOpacity(1),
+                  )
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
             ),
-            Text(webtoon.title),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
           ],
         );
       },
