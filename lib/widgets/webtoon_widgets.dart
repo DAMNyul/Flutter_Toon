ebcdcf49083163527bbac5f9504eface0f52c07e
김담율 <s24001@gsm.hs.kr>
2024-09-02 20:55:49 +0900

    오늘 그만할래

--- lib/widgets/webtoon_widgets.dart
+++ lib/widgets/webtoon_widgets.dart
@@ -11,39 +11,42 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 250,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15,
-                offset: const Offset(10, 10),
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        children: [
+          Container(
+            width: 250,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15,
+                  offset: const Offset(10, 10),
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ],
+            ),
+            child: Image.network(
+              thumb,
+              headers: const {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+              },
+            ),
           ),
-          child: Image.network(
-            thumb,
-            headers: const {
-              "User-Agent":
-                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-            },
+          const SizedBox(
+            height: 10,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22,
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
